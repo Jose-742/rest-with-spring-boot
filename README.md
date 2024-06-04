c21e52c08d2fd6baec019ebd0cb573ba1254286d
Jose-742 <josenatalrb@gmail.com>
2024-06-03 23:21:30 -0300

    Adding migration support with Flyway

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rest-with-spring-boot
+
+* Run the spring boot project through the terminal, ignoring the tests
+* mvn clean package spring-boot:run -DsKipTests
+
+
+* Run project migrations
+* mvn flyway:migrate
---
+++ src/main/resources/db/migration/V2__Insert_Table_Person.sql
@@ -0,0 +1,5 @@
+-- Insert table person
+INSERT INTO public.person (address, first_name, gender, last_name) VALUES
+           ('Quadra 405 norte AL 6', 'Jose Natal', 'Male', 'Rodrigues Bento'),
+           ('Quadra 505 norte AL 9', 'Gustavo Patricio', 'Male', 'Rodrigues Bento'),
+           ('Quadra 308 norte AL 8', 'Maria Clara', 'Female', 'Rodrigues Bento');
