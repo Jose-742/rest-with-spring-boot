fc04f88b94140b2272da674e42d41be1342d6000
Jose-742 <josenatalrb@gmail.com>
2024-07-10 20:29:12 -0300

    Custom JSON Serialization

--- src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
+++ src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
@@ -1,5 +1,8 @@
 package br.com.erudio.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.*;
 import java.io.Serializable;
 
@@ -8,11 +11,16 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@JsonPropertyOrder({"id", "address", "firstName", "lastName", "gender"}) // ordenando os atributos
 public class PersonVO implements Serializable {
 
     private Long id;
+
+    @JsonProperty("first_name") //Alterando nome do atributo
     private String firstName;
     private String lastName;
     private String address;
+
+    @JsonIgnore //ignorando atributo json
     private String gender;
 }
