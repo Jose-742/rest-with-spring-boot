eced6fbd32c4da73eaddac0c2017d56ab0a5a0bd
Jose-742 <josenatalrb@gmail.com>
2024-12-02 21:51:50 -0300

    Habilitando CORS de Forma Global

--- src/main/java/br/com/erudio/config/OpenApiConfig.java
+++ src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -14,7 +14,7 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("RESTful API with Java 18 and Spring Boot 3")
+                        .title("RESTful API with Java 21 and Spring Boot 3")
                         .version("v1")
                         .description("Some description")
                         .termsOfService("https://github.com/Jose-742/rest-with-spring-boot/tree/main")
--- src/main/java/br/com/erudio/config/WebConfig.java
+++ src/main/java/br/com/erudio/config/WebConfig.java
@@ -1,10 +1,12 @@
 package br.com.erudio.config;
 
 import br.com.erudio.serialization.converter.YamlJackson2HttpMesageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,6 +15,9 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -43,5 +48,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
        converters.add(new YamlJackson2HttpMesageConverter());
 
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+       var allowedOrigins = corsOriginPatterns.split(",");
+       registry.addMapping("/**")
+               //.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") setando por verbos
+               .allowedMethods("*") // todos
+               .allowedOrigins(allowedOrigins)
+       .allowCredentials(true);
     }
 }
