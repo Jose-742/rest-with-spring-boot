b75068cfc9110907fdf5822007b49085601bedf6
Jose-742 <josenatalrb@gmail.com>
2024-12-24 14:39:24 -0300

    Implementando Ordenação com Direction

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -14,6 +14,7 @@ import br.com.erudio.util.MediaType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,9 +43,13 @@ public class PersonController {
         })
     public ResponseEntity<Page<PersonVO>> findAll(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "limit", defaultValue = "12") Integer limit) {
+            @RequestParam(value = "size", defaultValue = "12") Integer size,
+            @RequestParam(value = "direction", defaultValue = "asc") String direction) {
 
-        Pageable pageable = PageRequest.of(page, limit);
+        var sortDirection = "desc".equalsIgnoreCase(direction)
+                ? Sort.Direction.DESC : Sort.Direction.ASC;
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
         return ResponseEntity.ok(personServices.findAll(pageable));
     }
 
