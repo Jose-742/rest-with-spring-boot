18e1592c52ea3f44138f3b4119dcc10a1aac7958
Jose-742 <josenatalrb@gmail.com>
2024-12-27 20:15:24 -0300

    Implementando o Download de Arquivos

--- src/main/java/br/com/erudio/controllers/FileController.java
+++ src/main/java/br/com/erudio/controllers/FileController.java
@@ -3,12 +3,14 @@ package br.com.erudio.controllers;
 import br.com.erudio.data.vo.v1.UploadFileResponseVO;
 import br.com.erudio.services.FileStorageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -46,6 +48,30 @@ public class FileController {
         return Arrays.stream(files)
                 .map(this::uploadFile)
                 .collect(Collectors.toList());
+    }
+
+
+    @GetMapping("/downloadFile/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename, HttpServletRequest request) {
+        log.info("Reading a file on disk");
+
+        Resource resource = service.loadFileAsResource(filename);
+        String contentType = "";
+
+        try{
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        }catch (Exception e){
+            log.info("Could not determine file type!");
+        }
+
+        if(contentType.isBlank()) contentType = "application/octet-stream";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(
+                        HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 }
--- src/main/java/br/com/erudio/services/FileStorageService.java
+++ src/main/java/br/com/erudio/services/FileStorageService.java
@@ -2,7 +2,10 @@ package br.com.erudio.services;
 
 import br.com.erudio.config.FileStorageConfig;
 import br.com.erudio.exceptions.FileStorageException;
+import br.com.erudio.exceptions.MyFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -44,4 +47,16 @@ public class FileStorageService {
             throw new FileStorageException("Could not store file "+ fileName + ". Please try again!", e);
         }
     }
+
+    public Resource loadFileAsResource(String filename) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(filename).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) return resource;
+            else throw new MyFileNotFoundException("File not found");
+        } catch (Exception e) {
+            throw new MyFileNotFoundException("File not found" + filename, e);
+        }
+    }
+
 }
