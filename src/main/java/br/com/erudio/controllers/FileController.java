7ea80a48a4612fc3abf3814e52854892b3ae1ca7
Jose-742 <josenatalrb@gmail.com>
2024-12-27 19:58:05 -0300

    Implementando o Upload de Vários Arquivos Simultaneamente

---
+++ src/main/java/br/com/erudio/controllers/FileController.java
@@ -0,0 +1,51 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.data.vo.v1.UploadFileResponseVO;
+import br.com.erudio.services.FileStorageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Slf4j
+@Tag(name = "File Endpoint")
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+
+    @Autowired
+    private FileStorageService service;
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponseVO uploadFile(@RequestParam("file")  MultipartFile file) {
+        log.info("Storing file to disk");
+
+        var filename = service.storeFile(file);
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/file/v1/downloadFile/")
+                .path(filename)
+                .toUriString();
+
+        return new UploadFileResponseVO(filename, fileDownloadUri, file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files")  MultipartFile[] files) {
+        log.info("Storing files to disk");
+
+        return Arrays.stream(files)
+                .map(this::uploadFile)
+                .collect(Collectors.toList());
+    }
+
+}
